44a187a5cd5f23b08d5324988b9dea4e82b612f3
XWT <2296361352@qq.com>
2021-12-22 00:11:42 +0800

    emmmm加了UI和死亡判定

--- Assets/Scripts/Bird.cs
+++ Assets/Scripts/Bird.cs
@@ -7,18 +7,47 @@ public class Bird : MonoBehaviour
     public float v = 1;
     private Rigidbody2D rb;
     public GameManager gameManager;
+    private Transform birdTransformbefore;
+    private Transform birdTransformafter;
+    private bool gameStart;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        //gameStart = GetComponent<GameManager>().gameStart;
+        birdTransformbefore = transform;
+        birdTransformafter = transform;
     }
 
     // Update is called once per frame
     void Update()
     {
+
         if (Input.GetMouseButtonDown(0))
         {
             rb.velocity = Vector2.up * v;
+
+        };
+        
+    }
+    void earlyUpdate()
+    {
+        birdTransformbefore = transform;
+    }
+    void lateUpdate()
+    {
+        if (birdTransformafter.position.y > birdTransformbefore.position.y)
+        {
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0, 0, 45), 0.05f);
+        }
+        if (birdTransformafter.position.y < birdTransformbefore.position.y)
+        {
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0, 0, -45), 0.05f);
+        }
+        birdTransformafter = transform;
+        if(birdTransformbefore != birdTransformafter)
+        {
+            Debug.Log(" not equal");
         }
     }
     private void OnCollisionEnter2D(Collision2D collision)
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,11 +7,12 @@ public class GameManager : MonoBehaviour
 {
     public GameObject gameStartCanvas;
     public GameObject gameOverCanvas;
-
+    
+    public bool gameStart=false;
     // Start is called before the first frame update
     void Start()
     {
-        //Time.timeScale = 0;
+        Time.timeScale = 0;
     }
 
     // Update is called once per frame
@@ -22,17 +23,19 @@ public class GameManager : MonoBehaviour
     public void GameOver()
     {
         gameOverCanvas.SetActive(true);
+        
         Time.timeScale = 0;
     }
     public void Replay()
     {
         SceneManager.LoadScene(0);
+        Time.timeScale = 1;
         
     }
     public void GameStart()
     {
+        gameStartCanvas.SetActive(false); 
         Time.timeScale = 1;
-        gameStartCanvas.SetActive(false);
-        
+        gameStart = true;
     }
 }
